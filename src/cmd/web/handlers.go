47a060bdca51928a05a4279e35634c33f8f49138
Colby Smith <colbysmith99@hotmail.com>
2026-07-06 16:14:35 +0100

    feat: home page handler html integration

--- src/cmd/web/handlers.go
+++ src/cmd/web/handlers.go
@@ -2,6 +2,8 @@ package main
 
 import (
     "fmt"
+    "html/template"
+    "log"
     "net/http"
     "strconv"
 )
@@ -11,6 +13,19 @@ func home (w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
+    }
+
+    ts, err := template.ParseFiles("../src/ui/html/pages/home.tmpl")
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
+        return
+    }
+
+    err = ts.Execute(w, nil)
+    if err != nil {
+        log.Println(err.Error())
+        http.Error(w, "Internal Server Error", 500)
     }
 
     w.Write([]byte("Hello from Snippetbox"))
